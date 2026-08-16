ccaad7feaca8934f9e2a3c1421e178b5893f17f4
Ridhesh927 <ridheshmahajan@gmail.com>
2026-08-16 20:29:53 +0530

    cicd errors

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
 const rateLimit = require('express-rate-limit');
 const { initDB } = require('./src/config/db');
 require('dotenv').config();
@@ -48,8 +47,6 @@ app.set('socketio', io);
 
 // Middleware
 app.use(helmet());
-// codeql[js/missing-token-validation]
-app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
